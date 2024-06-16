14380667360a9327013cfc4584ce02a88a1af3cf
WenHao2130 <WenHao2130@outlook.com>
2024-06-16 09:36:25 +0800

    arch: arm64: kernel: fix incorrect call to pr_warn function

--- arch/arm64/kernel/psci.c
+++ arch/arm64/kernel/psci.c
@@ -102,13 +102,13 @@ static int cpu_psci_cpu_kill(unsigned int cpu)
 	do {
 		err = psci_ops.affinity_info(cpu_logical_map(cpu), 0);
 		if (err == PSCI_0_2_AFFINITY_LEVEL_OFF) {
-			pr_debug("CPU%d killed.\n", cpu);
+			pr_debug("CPU%d killed (polled %d ms)\n", cpu,
+				jiffies_to_msecs(jiffies - start));
 			return 0;
 		}
 
-		msleep(10);
-		pr_debug("Retrying again to check for CPU kill\n");
-	}
+		usleep_range(100, 1000);
+	} while (time_before(jiffies, end));
 
 	pr_warn("CPU%d may not have shut down cleanly (AFFINITY_INFO reports %d)\n",
 			cpu, err);

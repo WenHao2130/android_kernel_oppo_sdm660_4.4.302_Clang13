90b886eed6302ac853b53761d777b3a55c4ac170
WenHao2130 <WenHao2130@outlook.com>
2024-06-23 11:54:52 +0800

    upload prebuilt anykernel3

---
+++ anykernel.sh
@@ -0,0 +1,34 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=
+do.devicecheck=0
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=maguro
+device.name2=toro
+device.name3=toroplus
+device.name4=
+device.name5=
+supported.versions=
+'; } # end properties
+
+# shell variables
+block=boot;
+is_slot_device=auto;
+ramdisk_compression=none;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel install
+split_boot;
+
+flash_boot;
+## end install
+
